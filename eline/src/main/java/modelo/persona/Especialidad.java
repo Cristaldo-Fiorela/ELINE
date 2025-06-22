89a8635f0bb5fe6f71b651b466829c44e472e96b
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-06-22 16:15:05 -0300

    clase personal con atributos, getter, setter y constructores

--- eline/src/main/java/modelo/persona/Especialidad.java
+++ eline/src/main/java/modelo/persona/Especialidad.java
@@ -1,5 +1,30 @@
 package modelo.persona;
 
 public class Especialidad {
-    
+    private int ID_Especialidad;
+    private String descripcion;
+
+    public Especialidad() {
+    }
+
+    public Especialidad(int ID_Especialidad, String descripcion) {
+        this.ID_Especialidad = ID_Especialidad;
+        this.descripcion = descripcion;
+    }
+
+    public int getID_Especialidad() {
+        return ID_Especialidad;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setID_Especialidad(int ID_Especialidad) {
+        this.ID_Especialidad = ID_Especialidad;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
 }
--- eline/src/main/java/modelo/persona/Personal.java
+++ eline/src/main/java/modelo/persona/Personal.java
@@ -1,5 +1,36 @@
 package modelo.persona;
 
-public class Personal {
-    
+public class Personal extends Persona {
+    private int ID_Especialidad; // fk de especialidad ID
+    private Especialidad especialidad; // acceso a objeto entero de especialidad. Se puede asignar un objeto entero desde el uso de este atributo (1, "Enfermero")
+
+    public Personal() {
+        super(); // llama a constructor de Persona
+    }
+
+    public Personal(int ID_Especialidad) {
+        this.ID_Especialidad = ID_Especialidad;
+    }
+
+    public Personal(int ID_Especialidad, Especialidad especialidad, String DNI, String nombre, String apellido, String telefono, String correo) {
+        super(DNI, nombre, apellido, telefono, correo);
+        this.ID_Especialidad = ID_Especialidad;
+        this.especialidad = especialidad;
+    }
+
+    public int getID_Especialidad() {
+        return ID_Especialidad;
+    }
+
+    public Especialidad getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setID_Especialidad(int ID_Especialidad) {
+        this.ID_Especialidad = ID_Especialidad;
+    }
+
+    public void setEspecialidad(Especialidad especialidad) {
+        this.especialidad = especialidad;
+    }
 }
