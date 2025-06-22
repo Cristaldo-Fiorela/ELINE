2ef9c0fdc550bf56bbf02f1c2a8756528ccae8aa
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-06-22 18:22:06 -0300

    ckases sector y turno

--- eline/src/main/java/modelo/guardia/Sector.java
+++ eline/src/main/java/modelo/guardia/Sector.java
@@ -1,5 +1,40 @@
 package modelo.guardia;
 
 public class Sector {
-    
+    private int id_sector;
+    private String descripcion;
+    private String ubicacion;
+
+    public Sector() {
+    }
+
+    public Sector(int id_sector, String descripcion, String ubicacion) {
+        this.id_sector = id_sector;
+        this.descripcion = descripcion;
+        this.ubicacion = ubicacion;
+    }
+
+    public int getId_sector() {
+        return id_sector;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setId_sector(int id_sector) {
+        this.id_sector = id_sector;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
 }
--- eline/src/main/java/modelo/guardia/Turno.java
+++ eline/src/main/java/modelo/guardia/Turno.java
@@ -1,5 +1,50 @@
 package modelo.guardia;
 
 public class Turno {
-    
+    private int id_turno;
+    private String descripcion;
+    private int hora_inicio;
+    private int hora_salida;
+
+    public Turno() {
+    }
+
+    public Turno(int id_turno, String descripcion, int hora_inicio, int hora_salida) {
+        this.id_turno = id_turno;
+        this.descripcion = descripcion;
+        this.hora_inicio = hora_inicio;
+        this.hora_salida = hora_salida;
+    }
+
+    public int getId_turno() {
+        return id_turno;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public int getHora_inicio() {
+        return hora_inicio;
+    }
+
+    public int getHora_salida() {
+        return hora_salida;
+    }
+
+    public void setId_turno(int id_turno) {
+        this.id_turno = id_turno;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setHora_inicio(int hora_inicio) {
+        this.hora_inicio = hora_inicio;
+    }
+
+    public void setHora_salida(int hora_salida) {
+        this.hora_salida = hora_salida;
+    }
 }
