c469b53ef34ff88dbcc4d887491605b6d2b867e1
daianayb <96448936+daianayb@users.noreply.github.com>
2025-06-25 03:40:01 -0300

    correccion clase guardia2.0

--- eline/src/main/java/modelo/guardia/Guardia.java
+++ eline/src/main/java/modelo/guardia/Guardia.java
@@ -15,69 +15,75 @@ import java.time.LocalDate;
 public class Guardia {
     private int id_guardia;
     private int id_persona;  
-    private int id_turno;
+    private String turno;
     private String descripcionSector;
-    
+    private String ubicacionSector;  
     private LocalDate fecha;
-
 
     public Guardia() {
     }
 
-    public Guardia(int id_guardia, int id_ambulancia, int id_sector, LocalDate fecha, int id_turno) {
+    public Guardia(int id_guardia, int id_persona, int id_turno, String descripcionSector, String ubicacionSector, LocalDate fecha) {
         this.id_guardia = id_guardia;
-        this.id_ambulancia = id_ambulancia;
-        this.id_sector = id_sector;
+        this.id_persona = id_persona;
+        this.turno = turno;
+        this.descripcionSector = descripcionSector;
+        this.ubicacionSector = ubicacionSector;
         this.fecha = fecha;
-        this.id_turno = id_turno;
-    }
-
-    public void setId_guardia(int id_guardia) {
-        this.id_guardia = id_guardia;
-    }
-
-    public void setId_ambulancia(int id_ambulancia) {
-        this.id_ambulancia = id_ambulancia;
-    }
-
-    public void setId_sector(int id_sector) {
-        this.id_sector = id_sector;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
-
-    public void setId_turno(int id_turno) {
-        this.id_turno = id_turno;
     }
 
     public int getId_guardia() {
         return id_guardia;
     }
 
-    public int getId_ambulancia() {
-        return id_ambulancia;
+    public void setId_guardia(int id_guardia) {
+        this.id_guardia = id_guardia;
     }
 
-    public int getId_sector() {
-        return id_sector;
+    public int getId_persona() {
+        return id_persona;
+    }
+
+    public void setId_persona(int id_persona) {
+        this.id_persona = id_persona;
+    }
+
+    public String getId_turno() {
+        return turno;
+    }
+
+    public void setId_turno(int id_turno) {
+        this.turno = turno;
+    }
+
+    public String getDescripcionSector() {
+        return descripcionSector;
+    }
+
+    public void setDescripcionSector(String descripcionSector) {
+        this.descripcionSector = descripcionSector;
+    }
+
+    public String getUbicacionSector() {
+        return ubicacionSector;
+    }
+
+    public void setUbicacionSector(String ubicacionSector) {
+        this.ubicacionSector = ubicacionSector;
     }
 
     public LocalDate getFecha() {
         return fecha;
     }
 
-    public int getId_turno() {
-        return id_turno;
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
     }
-    
-    /*
-    //aca iria como en la tabla y el txt
+
     @Override
     public String toString() {
-        return  id_guardia + "," + persona + "," + sector + "," + turno + "," + ambulancia + "," + fecha_turno;
+        return  id_guardia + "," + id_persona + "," + turno + "," + descripcionSector + "," + ubicacionSector + "," + fecha;
     }
-    */
+
 
 }
